5db4fe8df9cdf29c3a401900f495afe58f14e1ee
shinypolaris <88910096+shinypolaris@users.noreply.github.com>
2024-08-17 03:54:19 +0800

    fix: segmentation fault in padzero() when syscall execve

--- fs/ioctl.c
+++ fs/ioctl.c
@@ -855,7 +855,6 @@ static int do_vfs_ioctl(struct file *filp, unsigned int fd,
 
 SYSCALL_DEFINE3(ioctl, unsigned int, fd, unsigned int, cmd, unsigned long, arg)
 {
-	// printk(KERN_INFO "Entering ioctl syscall fd: %u, cmd: %u, arg: %u\n", fd, cmd, arg);
 	struct fd f = fdget(fd);
 	int error;
 
